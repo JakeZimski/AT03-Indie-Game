cb7597a7763c214234a8e9aa7cd48ba0de9a86b8
JakeZimski <jakegarratt02@gmail.com>
2022-03-28 16:02:34 +0800

    main menu implemented however buttons do need to be fixed up soon

---
+++ AT03 Indie Game/Assets/Scripts/RandomScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomScript : MonoBehaviour
+{
+    public void DebugMessage(string message)
+    {
+        Debug.Log(message);
+    }
+
+}
